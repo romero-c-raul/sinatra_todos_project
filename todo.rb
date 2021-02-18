086a968b8d1b75a6adfa24de803a396a49a6a874
romero-c-raul <raulromerocano@aggienetwork.com>
2021-02-18 11:11:32 -0330

    Refactored code using Rubocop

--- todo.rb
+++ todo.rb
@@ -1,7 +1,9 @@
-require "bundler/setup"
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -12,42 +14,41 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View all the lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Renders the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
 # Return an error message if the name is invalid. Return nil if name is valid.
 def error_for_list_name(name)
   if !(1..100).cover? name.size                              # Validates character length
-    "The list name must be between 1 and 100 characters."
+    'The list name must be between 1 and 100 characters.'
   elsif session[:lists].any? { |list| list[:name] == name }  # Validates repeated titles
-    "List name must be unique."
-  else
-    nil
+    'List name must be unique.'
   end
 end
 
 # Creates a new list
-post "/lists" do
+post '/lists' do
   list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)       # Checking if error is truthy
 
-  if error = error_for_list_name(list_name)       # Checking if error is truthy
+  if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: [] }
-    session[:success] = "The list has been created."
-    redirect "/lists"
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
   end
 end
